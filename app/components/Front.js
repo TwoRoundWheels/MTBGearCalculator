032b62b146d727d368d890c4b7a34952d29539b9
Josh <bikeisgood2@gmail.com>
2017-02-09 13:47:20 -0700

    Added defaultGearNumbers prop to Front.js which is passed to GearSelectionForm.js, this should make the GearSelectionForm useable by Rear.js, by passing different props to it.

--- app/components/Front.js
+++ app/components/Front.js
@@ -2,10 +2,13 @@ var React = require('react');
 var GearSelectionForm = require('../components/GearSelectionForm');
 
 var Front = React.createClass({
+	defaultProps: {
+		defaultGearNumbers: [1, 2, 3]
+	},
 	getInitialState: function () {
 		return {
 			sizeOfGears: [22, 32, 44],
-			numberOfGears: 3
+			currentNumberOfGears: 3
 		};
 	},
 	handleTextChange: function (e) {
@@ -16,11 +19,11 @@ var Front = React.createClass({
 	},
 	handleRadioChange: function (e) {
 		var value = e.target.value;
-		this.setState({numberOfGears: value});
+		this.setState({currentNumberOfGears: value});
 	},
 	render: function() {
 		var HEIGHTMODIFIER = 5;
-		numberOfGears = this.state.numberOfGears;
+		var numberOfGears = this.state.currentNumberOfGears;
 		var gears = this.state.sizeOfGears.map(function(size, index) {
 			if(index + 1 <= numberOfGears) {
 				return <li key={index} style={{height: size * HEIGHTMODIFIER}}className="gear">{size}</li>
@@ -29,7 +32,12 @@ var Front = React.createClass({
 		});	
 		return (
 			<div>
-				<GearSelectionForm radioChange={this.handleRadioChange} textChange={this.handleTextChange} number={this.state.numberOfGears} sizes={this.state.sizeOfGears}/>
+				<GearSelectionForm 
+					radioChange={this.handleRadioChange} 
+					textChange={this.handleTextChange} 
+					number={this.state.currentNumberOfGears} 
+					sizes={this.state.sizeOfGears}
+					defaultGears={this.defaultProps.defaultGearNumbers}/>
 				<ul className="gears">
 					{gears}
 				</ul>
--- app/components/GearSelectionForm.js
+++ app/components/GearSelectionForm.js
@@ -2,26 +2,30 @@ var React = require('react');
 
 var GearSelectionForm = React.createClass({
 	render: function() {
-		textInputs = [];
+		var textInputs = [];
+		var radioButtons = [];
+		for (var i = 0; i < this.props.defaultGears.length; i++) {
+			radioButtons.push(
+					<label key={i}>
+						<input key={i} 
+							   type="radio" 
+							   name="number-of-gears" 
+							   value={this.props.defaultGears[i]} 
+							   defaultChecked={this.props.defaultGears[i] === this.props.number }/>
+						{this.props.defaultGears[i]}
+					</label>
+				);
+		}; 
+
 		for (var i = 0; i < this.props.number; i++) {
 			textInputs.push(<input key={i} name={i} type="text" value={this.props.sizes[i]} onChange={this.props.textChange} />);
 		};
+
 		return (
 			<div>
 			<form onChange={this.props.radioChange}>
 				<label>Number of gears:</label>
-				<label>
-					<input type="radio" name="number-of-gears" value="1" />
-					1
-				</label>
-				<label>
-					<input type="radio" name="number-of-gears"  value="2" />
-					2
-				</label>
-				<label>
-					<input type="radio" name="number-of-gears"  value="3" />
-					3
-				</label>
+				{radioButtons}
 			</form>
 			<form>
 				<p>
